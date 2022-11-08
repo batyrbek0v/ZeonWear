bdda884d2a9e0004313572b6c860b2d03df8bd0f
Эрлан Камбаров <erlankambarov94@gmail.com>
2022-11-08 14:57:04 +0600

    added slider in main page

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,78 @@
-// let swiper = new Swiper(".mySwiper", {
-//     grabCursor: true,
-//     slidesPerView: 5,
-//     spaceBetween: 24,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
+// DOM-ELEMENTS
+
+const $wrapper = document.querySelector('.main-category-block')
+
+
+// DOM-ELEMENTS
+
+
+
+
+const category = [
+	{
+		title: 'Бутсы'
+	},
+	{
+		title: 'Мини-футбол'
+	},
+	{
+		title: 'Фитнес'
+	},
+	{
+		title: 'Обувь'
+	},
+	{
+		title: 'Йога'
+	},
+	{
+		title: 'Майки и футболки'
+	},
+	{
+		title: 'Куртки и жилеты'
+	},
+	{
+		title: 'Спортивные костюмы'
+	},
+	{
+		title: 'Головной убор'
+	},
+	{
+		title: 'Мячи'
+	},
+	{
+		title: 'Бутсы'
+	},
+]
+
+
+
+window.addEventListener('load', () => {
+	const template = category.map(({ title }) => categoryTemplate(title)).join('')
+
+	$wrapper.innerHTML = template
+})
+
+
+const categoryTemplate = title => `<button class="main-category-btn">${title}</button>`
+
+
 
 let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    slidesPerView: 5,
-    loop: true,
-    keyboard: {
-        enabled: true,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+	slidesPerView: 1,
+	// spaceBetween: 24,
+	slidesPerView: 5,
+	centeredSlides: true,
+	loop: true,
+	keyboard: {
+		enabled: true,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
 });
-// var swiper = new Swiper(".mySwiper", {
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: 5,
-//     spaceBetween: 24,
-//     slidesPerView: "auto",
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
